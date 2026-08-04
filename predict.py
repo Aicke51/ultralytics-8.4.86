ec6cfa4ce1da8ff14e6e9cb35ef3d5de84229178
UltralyticsAssistant <web@ultralytics.com>
2026-08-04 04:12:18 +0000

    Auto-format by https://ultralytics.com/actions

--- predict.py
+++ predict.py
@@ -1,8 +1,9 @@
 from ultralytics import YOLO
-#官方训练模型
-#yolo = YOLO(model='yolo26m.pt' , task='deteck')
-#result = yolo(source = 'people.jpg',save=True)
 
-#icon图标训练后模型
-yolo = YOLO(model='icon.pt' , task='deteck')
-result = yolo(source = 'screen',save=True)
+# 官方训练模型
+# yolo = YOLO(model='yolo26m.pt' , task='deteck')
+# result = yolo(source = 'people.jpg',save=True)
+
+# icon图标训练后模型
+yolo = YOLO(model="icon.pt", task="deteck")
+result = yolo(source="screen", save=True)
